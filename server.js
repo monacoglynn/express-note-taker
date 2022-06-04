66ce279b513fd5a80b301c742cbde680db798773
monacoglynn <96494895+monacoglynn@users.noreply.github.com>
2022-06-04 15:30:57 -0700

    found error and commented it out

--- server.js
+++ server.js
@@ -24,19 +24,17 @@ app.use(express.static('public'));
 //     res.sendFile(path.join(__dirname, '/public/index.html'))
 // });
 
-app.get('/api/notes', (req, res) => {
-    res.json(`${req.method} request received`);
-    console.info(`${req.method} request received`);
-    return res.json(notes);
-})
+// app.get('/api/notes', (req, res) => {
+//     res.json(`${req.method} request received`);
+//     return res.json(notes);
+// })
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
-//post requests
+// post requests
 app.post('/api/notes', (req, res) => {
-    console.info(`${req.method} request received to add new notes`);
     let response;
 
     if (req.body && req.body.title && req.body.text) {
